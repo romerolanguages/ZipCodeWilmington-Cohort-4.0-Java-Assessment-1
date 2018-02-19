73889c1f04bdbf7ffaf3ca9be8b7ce41c4ff15ef
Luis Romero <luisromero@zipcoders-MacBook-Pro-47.local>
2018-02-19 18:59:07 -0500

    finished part 5, have to go back and retest sections in part 2 annd part 3

--- src/main/java/com/zipcodewilmington/assessment1/part5/Palindrome.java
+++ src/main/java/com/zipcodewilmington/assessment1/part5/Palindrome.java
@@ -1,8 +1,39 @@
 package com.zipcodewilmington.assessment1.part5;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Palindrome {
 
     public Integer countPalindromes(String input){
-        return null;
+
+        // get arraylist of substrings first:
+        ArrayList<String> stringArrayList = new ArrayList<>();
+
+        for (int i = 0; i < input.length(); i++) {
+            for (int j = i; j < input.length(); j++) {
+                stringArrayList.add(input.substring(i, j + 1));
+            }
+        }
+
+        //System.out.println(Arrays.toString(stringArrayList.toArray()));
+        String test = "hello";
+        //System.out.println(test + " " + reversed);
+
+        // get number of palindromes from the above arraylist
+
+        int palindromeCount = 0;
+        StringBuilder sb = new StringBuilder();
+
+        for (String string : stringArrayList) {
+            if ( string.equals(sb.append(string).reverse().toString()) ) {
+                palindromeCount++;
+            }
+            sb.setLength(0);
+        }
+
+        //String reversed = sb.append(test).reverse().toString();
+
+        return palindromeCount;
     }
 }
