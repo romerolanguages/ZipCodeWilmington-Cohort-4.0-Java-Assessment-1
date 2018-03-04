8a35876475a2e5f005ae399c2fee7b8aad6c1aac
Luis Romero <luisromero@zipcoders-MacBook-Pro-47.local>
2018-03-04 13:20:54 -0500

    BasicStringUtils updated

--- src/main/java/com/zipcodewilmington/assessment1/part1/BasicStringUtils.java
+++ src/main/java/com/zipcodewilmington/assessment1/part1/BasicStringUtils.java
@@ -9,17 +9,10 @@ public class BasicStringUtils {
      * @return string with identical content, and the first character capitalized
      */
     public static String camelCase(String str) {
-
         StringBuilder sb = new StringBuilder();
-
-        String allUpperCase = str.toUpperCase();
-
-        sb.append(allUpperCase.charAt(0) + str.substring(1, str.length()));
-
-        //System.out.println(sb.toString());
+        sb.append(str.substring(0, 1).toUpperCase() + str.substring(1));
         return sb.toString();
     }
-
 
     /**
      * @param str string input from client
@@ -27,69 +20,45 @@ public class BasicStringUtils {
      */
     public static String reverse(String str) {
         StringBuilder sb = new StringBuilder(str);
-
-        sb.reverse();
-        //System.out.println(sb.toString());
-
-        return sb.toString();
+        return sb.reverse().toString();
     }
-
 
     /**
      * @param str string input from client
      * @return string with identical contents, in reverse order, with first character capitalized
      */
     public static String reverseThenCamelCase(String str) {
-        StringBuilder sb = new StringBuilder(str);
 
-        String reverseStr = sb.reverse().toString();
-        String allUpperCaseReverseStr = reverseStr.toUpperCase();
-
-        sb.setLength(0);
-
-        sb.append(allUpperCaseReverseStr.charAt(0) + reverseStr.substring(1, reverseStr.length()));
-        //System.out.println(sb.toString());
-
-        return sb.toString();
+        String reversedString = reverse(str);
+        String reversedFirstLetterUppercased = camelCase(reversedString);
+        return reversedFirstLetterUppercased;
     }
-
 
     /**
      * @param str a string input from user
      * @return string with identical contents excluding first and last character
      */
     public static String removeFirstAndLastCharacter(String str) {
-
         StringBuilder sb = new StringBuilder(str);
-
         sb.deleteCharAt(0);
         sb.deleteCharAt(sb.length() - 1);
-
-        //System.out.println(sb.toString());
-
         return sb.toString();
     }
-
 
     /**
      * @param str a string input from user
      * @return string with identical characters, each with opposite casing
      */
     public static String invertCasing(String str) {
-
         char[] chars = str.toCharArray();
-
         for (int i = 0; i < chars.length; i++) {
             char c = chars[i];
-            if (Character.isUpperCase(c)) {
-                chars[i] = Character.toLowerCase(c);
-            } else if (Character.isLowerCase(c)) {
+            if (Character.isLowerCase(c)) {
                 chars[i] = Character.toUpperCase(c);
+            } else if (Character.isUpperCase(c)) {
+                chars[i] = Character.toLowerCase(c);
             }
         }
-
-        //System.out.println(new String(chars));
-
         return new String(chars);
     }
 }
